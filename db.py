d524e319cc3d103565481a55987b985e9f420520
Mehmet Altıok <121510181+13Mehmet13@users.noreply.github.com>
2025-07-21 14:18:41 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import psycopg2
-import traceback
-from psycopg2.extras import DictCursor  # ✅ EKLENDİ
+from psycopg2.extras import DictCursor
 
 USER = "postgres.vpjltarimjmmvtryzpgl"
 PASSWORD = "13MehMet2003."
@@ -8,14 +7,12 @@ HOST = "aws-0-eu-central-1.pooler.supabase.com"
 PORT = "5432"
 DBNAME = "postgres"
 
-try:
-    conn = psycopg2.connect(
-        user=USER, password=PASSWORD,
-        host=HOST, port=PORT,
-        dbname=DBNAME
+def get_conn():
+    return psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME,
+        cursor_factory=DictCursor  # İsteğe bağlı: sözlük gibi erişim sağlar
     )
-    cursor = conn.cursor(cursor_factory=DictCursor)  # ✅ DİKKAT! Buraya dikkat
-    print("✅ Veritabanı bağlantısı başarılı!")
-except Exception:
-    print("❌ Veritabanına bağlanılamadı:")
-    traceback.print_exc() 
